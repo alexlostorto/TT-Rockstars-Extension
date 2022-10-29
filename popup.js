fe1cf323aa1aed202e8d9f91a7f29f6689325335
Alex Lo Storto <lostorto.business@gmail.com>
2022-10-29 22:23:04 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,6 +1,23 @@
 startButton = document.querySelector('#start');
 
 startButton.addEventListener('click', function() {
+    minElement = document.querySelector('.min');
+    maxElement = document.querySelector('.max');
+
+    let min = 0
+    let max = 0
+
+    if ((minElement.value != '') && (maxElement.value != '')) {
+        min = minElement.value;
+        max = maxElement.value;
+    } else {
+        min = 300;
+        max = 500;
+    }
+
+    console.log(min);
+    console.log(max);
+
     if (startButton.textContent == 'Start') {
         startButton.textContent = 'Stop';
     } else {
@@ -8,6 +25,6 @@ startButton.addEventListener('click', function() {
     }
 
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {message: "Start"});
+        chrome.tabs.sendMessage(tabs[0].id, {message: "Start", min: min, max: max});
       });
 });
